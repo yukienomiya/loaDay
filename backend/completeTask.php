b34cd114d131e9411e96aa088be4b27135b64d8f
yukienomiya <yn.yuki98@gmail.com>
2020-06-01 11:57:11 +0200

    Create completeTask.php

---
+++ backend/completeTask.php
@@ -0,0 +1,24 @@
+<?php
+    session_start();
+    require_once('connection.php');
+
+    // prendo i dati e aggiorno l'attributo completato del task nel db
+    if(isset($_POST["taskID"]))
+    {
+        $id = $_POST["taskID"];
+        $completed = $_POST["taskCompleted"];
+        /* $user = $_SESSION['email']; */
+
+        $sql = "UPDATE tasks SET completato = '$completed' WHERE tasks.id = '$id'";
+    
+        if (!$conn -> query($sql)) { // se qualcosa e andato storto
+          die($mysqli -> error);
+        }
+    }
+    else
+    {
+        header('HTTP/1.1 401 Unauthorized');
+    }
+
+    mysqli_close($conn);
+?>
