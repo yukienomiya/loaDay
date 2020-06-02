46eb9db3da3969385604a26ef9901390575d8368
yukienomiya <yn.yuki98@gmail.com>
2020-06-03 00:44:07 +0200

    Update index page UI

--- frontend/index.php
+++ frontend/index.php
@@ -3,68 +3,43 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>loaDay - Home Page</title>
+		<title>loaDay - Benvenuto</title>
 		<!-- Bootstrap core CSS -->
 		<link href="main.css" rel="stylesheet">
-		<link href="style.css" rel="stylesheet">
-	  </head>
-	  <body>
-		<nav class="navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar">
-		  <div class="container">
-			<a class="navbar-brand" href="index.php">
-			  <strong>loaDay</strong>
-			</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-			  aria-expanded="false" aria-label="Toggle navigation">
-			  <span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-			  <!-- Left -->
-			  <ul class="navbar-nav mr-auto">
-			  </ul>
-			  <!-- Right -->
-			  <ul class="navbar-nav nav-flex-icons ml-auto">
-				<li class="nav-item">
-				  <a href="registration.html" class="nav-link border border-light rounded">
-					Registrati!
-				  </a>
-				</li>
-			  </ul>
-			</div>
-		  </div>
-		</nav>
+    <link href="style.css" rel="stylesheet">
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+		<script src="js/index.js"></script>
+	</head>
+	<body>
 	  
 		<!-- Full Page Intro -->
-		<div class="view" style="background-image: url('../media/sfondo.png'); background-repeat: no-repeat; background-size: cover;">
+		<div class="view index-bg">
 		  <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
-			<div class="container">
-			  <div class="row">
-				<div class="col-md-6 mb-4 white-text text-center text-md-left">
+			<div class="container d-flex">
+			  <div class="row justify-content-between align-self-center">
+				<div class="col-md-6 mb-4 text-center text-md-left">
 	  
-				  <h1 class="display-4 font-weight-bold">LoaDay</h1>
+				  <img src="../media/logoST.png" style="height: 150px;">
 	  
-				  <hr class="hr-light">
+				  <hr class="hr-dark">
 	  
-				  <p>
-					<strong>Ottimizza il tuo tempo e produci al massimo!</strong>
-				  </p>
+				  <p class="text-secondary">Pianifica i tuoi obiettivi e carica la tua giornata!</p>
 	  
-				  <p class="mb-4 d-none d-md-block">
-					<strong>Tramite questa home page meravigliosa potrai accedere e registrarti all'applicazione web che ti farà laureare in poco tempo!
-						Organizza la tua giornata nel migliore dei modi senza mai dimenticare ciò che devi fare.
-					</strong>
-				  </p>
+				  <p class="d-none d-md-block text-secondary">
+            loaDay ti offre una piattaforma dove annotare i tuoi obiettivi giornalieri e le tue imminenti scadenze, in modo da aumentare la tua produttività.
+            Completa tutti i task che ti sei prefissato e riempi la barra di caricamento della giornata!
+          </p>
 	  
-				<a class="btn btn-primary btn-lg" id="mostraNascosta">Leggi altro
-				  </a>
+				  <a class="d-none d-md-inline index-link" id="mostraNascosta">Scopri di più</a>
 	  
 				</div>
 				<div class="col-md-6 col-xl-5 mb-4">
 				  <div class="card">
-					<div class="card-header">
-						<h3 class="dark-grey-text text-center">
-							<strong>Accedi</strong>
-						  </h3>
+					<div class="card-header login-card-header">
+						<h3 class="text-center mb-0 font-weight-light">
+							Accedi
+						</h3>
 					</div>
 					<div class="card-body">
 					  <!-- Form -->
@@ -104,9 +79,9 @@
 
 						  
 						<div class="text-center mt-4">
-							<button id="login" type="submit" name="login" class="btn btn-primary">Invia</button>
+							<button id="login" type="submit" name="login" class="btn btn-accent mb-2">LOGIN</button>
 							<br>
-							<small><a href="PageSignIn/registration.html" class="btn-link">Non sei registrato? Clicca qui!</a></small>
+							<small><a href="/registration.html" class="btn-link">Non sei registrato? Clicca qui!</a></small>
 						</div>
 					  </form>
 					</div>
@@ -122,48 +97,36 @@
 			<section class="mt-5">
 			  <div class="row">
 				<div class="col-md-6 mb-4">
-					<div id="myCar" class="carousel slide carousel-fade" data-ride="carousel">
+					<div id="myCar" class="carousel slide carousel-slide" data-ride="carousel">
 					  <ol class="carousel-indicators">
 						<li data-target="#myCar" data-slide-to="0" class="active"></li>
 						<li data-target="#myCar" data-slide-to="1"></li>
 						<li data-target="#myCar" data-slide-to="2"></li>
 						<li data-target="#myCar" data-slide-to="3"></li>
-						<li data-target="#myCar" data-slide-to="4"></li>
 					  </ol>
 					  <div class="carousel-inner">
 						  <div class="carousel-item active">
-							  <img class="d-block w-100" src="../media/1.png" alt="First slide">
+							  <img class="d-block w-100" src="../media/taskICON.png" alt="First slide">
 							  <div class="carousel-caption d-none d-md-block">
-								  <h5>Home Page</h5>
-									<p>La pagina inziale dove ti trovi ora</p>
+								  <h5>TO-DO LIST</h5>
 							  </div>
 						  </div>
 						  <div class="carousel-item">
-							  <img class="d-block w-100" src="../media/2.png" alt="Second slide">
+							  <img class="d-block w-100" src="../media/dlICON.png" alt="Second slide">
 							  <div class="carousel-caption d-none d-md-block">
-								  <h5>Pagina Principale</h5>
-									<p>Dove potrai visualizzare, modificare ed eliminare tasks e deadlines</p>
+								  <h5>DEADLINES</h5>
 							  </div>
 						  </div>
 						  <div class="carousel-item">
-							  <img class="d-block w-100" src="../media/3.png" alt="Third slide">
+							  <img class="d-block w-100" src="../media/profileICON.png" alt="Third slide">
 							  <div class="carousel-caption d-none d-md-block">
-								  <h5>About Us</h5>
-									<p>Qui troverai una descrizione dei componenti del gruppo di questo progetto!</p>
+								  <h5>PROFILO</h5>
 							  </div>
 						  </div>
 						  <div class="carousel-item">
-							  <img class="d-block w-100" src="../media/4.png" alt="Third slide">
+							  <img class="d-block w-100" src="../media/aboutusICON.png" alt="Third slide">
 							  <div class="carousel-caption d-none d-md-block">
-								  <h5>Profilo</h5>
-									<p>LOREM IPSUM STO PIPOLO</p>
-							  </div>
-						  </div>
-						  <div class="carousel-item">
-							  <img class="d-block w-100" src="../media/5.png" alt="Third slide">
-							  <div class="carousel-caption d-none d-md-block">
-							  	<h5>Pagina di Registrazione</h5>
-								<p>Pronto a sfruttare a pieno la tua giornata?!</p>
+								  <h5>ABOUT US</h5>
 							  </div>
 						  </div>
 					  </div>
@@ -179,29 +142,27 @@
 	
 			  </div>
 				<div class="col-md-6 mb-4">
-				  <h3 class="h3 mb-3">Benvenuto alla descrizione segreta</h3>
-				  <p>LoaDay nasce dall'idea di 3 ragazzi che dovevano svolgere l'esame di linguaggi e tecnologie per il web durante l'anno accademico 2019/2020</p>
-				  <p>Leggi i dettagli in basso per scoprire da quale pagine è formata LoaDay.</p>
-				  <p>Questa è soltanto una breve descrizione, <a href="PageSignIn/registration.html">registrati</a> e visita la pagina about us per saperne ancora di più!</p>
+				  <h3 class="h3 mb-3">Una sbirciatina alle funzionalità</h3>
+				  <p>Se deciderai di <a href="/registration.html">registrati</a> a loaDay, avrai accesso a queste pagine:</p>
+          <p>Home, dove potrai annotare i tuoi task per oggi, domani o per il futuro. Inoltre, troverai anche la sezione Deadlines, dove inserire le scadenze importanti da ricordare.</p>
+          <p>Profilo, dove potrai modificare i tuoi dati personali.</p>
+          <p>About Us, dove scoprirai di più su chi ha creato loaDay.</p>
 
 				  <hr>
 
 				  <p>
 					  <ol class="list-group">
 						  <li class="list-group-item" data-target="#myCar" data-slide-to="0">
-							<strong>Home Page</strong>
+							<strong>To-do list</strong>
 						  </li>
 						  <li class="list-group-item" data-target="#myCar" data-slide-to="1">
-							<strong>Pagina Principale</strong>
+							<strong>Deadlines</strong>
 						  </li>
 						  <li class="list-group-item" data-target="#myCar" data-slide-to="2">
-							<strong>About Us</strong>
-						  </li>
-						  <li class="list-group-item" data-target="#myCar" data-slide-to="3">
 							<strong>Profilo</strong>
 						  </li>
-						  <li class="list-group-item" data-target="#myCar" data-slide-to="4">
-							<strong>Pagina di Registrazione</strong>
+						  <li class="list-group-item" data-target="#myCar" data-slide-to="3">
+							<strong>About Us</strong>
 						  </li>
 					  </ol>					
 				  </p>
@@ -211,16 +172,5 @@
 		  </div>
 		</main>
 
-		<footer class="page-footer text-center font-small">
-		  <div class="footer-copyright py-3">
-				<div class="text-muted">
-					Made with ♥️.
-				</div>
-		  </div>
-		</footer>
-
-		<script src="node_modules/jquery/dist/jquery.min.js"></script>
-    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
-		<script src="js/index.js"></script>
-	  </body>
+	</body>
 </html>
